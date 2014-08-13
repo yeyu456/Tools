e95111edcc36d84ce34c71ca94c5501f1c77fc07
yuye <yeyu456@gmail.com>
2014-08-13 14:13:23 +0800

    Update setup.py

--- CDN_TEST/Source Code/setup.py
+++ CDN_TEST/Source Code/setup.py
@@ -8,8 +8,8 @@ build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base='Console'
-setup(  name = "鬼武者节点测试工具",
+setup(  name = "CDN节点测试工具",
         version = "1.2.0",
-        description = "GWZ CDN TEST",
+        description = "DN TEST",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("GWZ_CDN_TEST.py", base=base)])
+        executables = [Executable("CDN_TEST.py", base=base)])
